ee0e1e00ae51cf1f153d004952b00176bd6f3070
BooleanBob-fr <72748340+BooleanBob-fr@users.noreply.github.com>
2021-01-03 17:05:35 +0100

    Update showHands.js

--- commands/showHands.js
+++ commands/showHands.js
@@ -13,23 +13,28 @@ module.exports = {
 	execute(message, args) {
 
 		var msg = "";
-			var nb = 1;
-			for (i = 0; i < board.player.length; i++) {
-				board.hands[i].forEach(function(element){
-					if ( element.type == 5 ) {
-						msg+="```css\n"+nb+" => "+config.cardType[element.type - 1]+config.cardArcane[element.number - 1]+"```\n";
+		var nb = 1;
+		
+		if (board.deck == null) {
+			return message.channel.send("Le plateau n'est pas initialisé.", { split: true });
+		}
+		
+		for (i = 0; i < board.player.length; i++) {
+			board.hands[i].forEach(function(element){
+				if ( element.type == 5 ) {
+					msg+="```css\n"+nb+" => "+config.cardType[element.type - 1]+config.cardArcane[element.number - 1]+"```\n";
+				} else {
+					if ( config.cardType[element.type - 1].indexOf("Rouge") > -1 ) {
+						msg+="** "+nb+" => "+config.cardNumber[element.number - 1]+config.cardType[element.type - 1]+"**\n";
 					} else {
-						if ( config.cardType[element.type - 1].indexOf("Rouge") > -1 ) {
-							msg+="** "+nb+" => "+config.cardNumber[element.number - 1]+config.cardType[element.type - 1]+"**\n";
-						} else {
-							msg+=" "+nb+" => "+config.cardNumber[element.number - 1]+config.cardType[element.type - 1]+"\n";
-						}
+						msg+=" "+nb+" => "+config.cardNumber[element.number - 1]+config.cardType[element.type - 1]+"\n";
 					}
-					nb += 1;
-				});
-				message.author.send("__Contenu de la main de "+board.player[i]+" :__\n"+msg, { split: true });
-				nb = 1;
-				msg = "";
-			};
+				}
+				nb += 1;
+			});
+			message.author.send("__Contenu de la main de "+board.player[i]+" :__\n"+msg, { split: true });
+			nb = 1;
+			msg = "";
+		};
 	},
 };
