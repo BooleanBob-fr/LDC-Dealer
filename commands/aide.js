e343ce072e9b3801b77360cd35ba089268e58c32
BooleanBob-fr <72748340+BooleanBob-fr@users.noreply.github.com>
2021-02-06 19:18:07 +0100

    Update aide.js

--- commands/aide.js
+++ commands/aide.js
@@ -12,10 +12,10 @@ module.exports = {
 		const { commands } = message.client;
 
 		if (!args.length) {
-			data.push('\n Voici la liste des commandes disponibles pour les PJ :');
+			data.push('\n Voici la liste des commandes disponibles pour les PJ et leurs alias :');
 			data.push(commands.map(function(command){ if( command.user == 'PJ' ) { return command.name+" : "+command.aliases+"\n"; } }).join(''));
-			data.push('\n Voici la liste des commandes disponibles pour les MJ :');
-			data.push(commands.map(function(command){ if( command.user == 'MJ' ) { return command.name+"\n"; } }).join(''));
+			data.push('\n Voici la liste des commandes disponibles pour les MJ et leurs alias :');
+			data.push(commands.map(function(command){ if( command.user == 'MJ' ) { return command.name+" : "+command.aliases+"\n"; } }).join(''));
 			data.push(`\nVous pouvez obtenir plus d'informations sur une commande via : \`${prefix}help [command name]\``);
 
 			//return message.author.send(data, { split: true })
