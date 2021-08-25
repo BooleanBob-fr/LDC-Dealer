f7190e6dde6f1c896e72a722b35f75e20c002379
BooleanBob-fr <72748340+BooleanBob-fr@users.noreply.github.com>
2021-08-25 21:11:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const config = require('./config.json');
 const board = require('./board.js');
 
-const client = new Discord.Client({ intents: [Discord.Intents.ALL] });
+const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.ALL] });
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
