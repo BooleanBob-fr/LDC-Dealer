b5e256779a872adfbb5f5723559d34c03f95de42
BooleanBob-fr <72748340+BooleanBob-fr@users.noreply.github.com>
2021-09-01 23:17:56 +0200

    Update prendArcane.js
    Update send function for message.

--- commands/prendArcane.js
+++ commands/prendArcane.js
@@ -15,7 +15,7 @@ module.exports = {
 		
 		
 		if (board.deck == null) {
-			return message.channel.send(`${message.author}`+": Le plateau n'est pas initialisé.", { split: true });
+			return message.channel.send({ content: `${message.author}`+": Le plateau n'est pas initialisé."}, { split: true });
 		}
 		
 		var tmp = new Array();
@@ -63,9 +63,9 @@ module.exports = {
 		}
 		
 		if ( me_only == 0 ){
-			message.channel.send(`${message.author}`+": __Tirage d'une Arcane pour "+board.player[my_player_number]+" :__\n"+msg, { split: true });
+			message.channel.send({ content: `${message.author}`+": __Tirage d'une Arcane pour "+board.player[my_player_number]+" :__\n"+msg}, { split: true });
 		} else {
-			message.author.send(`${message.author}`+": __Tirage d'une Arcane pour "+board.player[my_player_number]+" :__\n"+msg, { split: true });
+			message.author.send({ content: `${message.author}`+": __Tirage d'une Arcane pour "+board.player[my_player_number]+" :__\n"+msg}, { split: true });
 		}
 	},
 };
